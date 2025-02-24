de9b854c90a2e901508f1bce56ee04c85eafa456
GazJack <greta.jackaite@gmail.com>
2025-02-24 19:38:11 +0200

    1 register ir 2 login u=baigti

--- cypress/e2e/automationExercizeTests/automationExercizeTests.cy.js
+++ cypress/e2e/automationExercizeTests/automationExercizeTests.cy.js
@@ -1,6 +1,6 @@
 // Test Case 1: Register User
 describe('Test Case 1: Register User', () => { // 1. Launch browser
-    it('Turėtų užregistruoti naują vartotoją', () => {
+    it("Let's start testing this site", () => {
         cy.visit('https://automationexercise.com'); // 2. Navigate to url 'http://automationexercise.com'
         cy.contains('Home').should('be.visible'); // 3. Verify that home page is visible successfully
         cy.contains('Signup / Login').click();  // 4. Click on 'Signup / Login' button
@@ -10,7 +10,7 @@ describe('Test Case 1: Register User', () => { // 1. Launch browser
         cy.get('[data-qa="signup-button"]').click(); // 7. Click 'Signup' button  
         cy.contains('Enter Account Information').should('be.visible'); // 8. Verify that 'ENTER ACCOUNT INFORMATION' is visible  
         cy.get('#id_gender1').click(); // 9. Fill details: Title, Name, Email, Password, Date of birth
-        cy.get('[data-qa="password"]').type('slaptazodis');
+        cy.get('[data-qa="password"]').type('zebras');
         cy.get('[data-qa="days"]').select('19');
         cy.get('[data-qa="months"]').select('January');
         cy.get('[data-qa="years"]').select('1991');
@@ -33,5 +33,47 @@ describe('Test Case 1: Register User', () => { // 1. Launch browser
         cy.contains('Delete Account').click(); // 17. Click 'Delete Account' button
         cy.contains('Account Deleted!').should('be.visible'); // 18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
         cy.get('[data-qa="continue-button"]').click();
+    });
+});
+
+// Test Case 2: Login User with correct email and password
+describe('Test Case 2: Login User with correct email and password', () => { // 1. Launch browser
+    it('Login with correct data', () => {
+        cy.visit('https://automationexercise.com'); // 2. Navigate to url 'http://automationexercise.com'
+        cy.contains('Home').should('be.visible'); // 3. Verify that home page is visible successfully
+        cy.contains('Signup / Login').click(); // 4. Click on 'Signup / Login' button
+        cy.contains('New User Signup!').should('be.visible'); 
+        cy.get('[data-qa="signup-name"]').type('zebriukas'); 
+        cy.get('[data-qa="signup-email"]').type('zebriukas@example.com');
+        cy.get('[data-qa="signup-button"]').click();   
+        cy.contains('Enter Account Information').should('be.visible'); 
+        cy.get('#id_gender1').click();
+        cy.get('[data-qa="password"]').type('zebriukas');
+        cy.get('[data-qa="days"]').select('19');
+        cy.get('[data-qa="months"]').select('January');
+        cy.get('[data-qa="years"]').select('1991');
+        cy.get('#newsletter').check(); 
+        cy.get('#optin').check();  
+        cy.get('[data-qa="first_name"]').type('Zebras'); 
+        cy.get('[data-qa="last_name"]').type('Zebrauskas');
+        cy.get('[data-qa="company"]').type('WildNature');
+        cy.get('[data-qa="address"]').type('Haugetuft 2');
+        cy.get('[data-qa="address2"]').type('Apartment 4');
+        cy.get('[data-qa="country"]').select('United States');
+        cy.get('[data-qa="state"]').type('California');
+        cy.get('[data-qa="city"]').type('Los Angeles');
+        cy.get('[data-qa="zipcode"]').type('789457');
+        cy.get('[data-qa="mobile_number"]').type('+4710965125');
+        cy.get('[data-qa="create-account"]').click(); 
+        cy.contains('Account Created!').should('be.visible'); 
+        cy.get('[data-qa="continue-button"]').click();
+        cy.contains('Logout').click();
+        cy.contains('Login to your account').should('be.visible'); // 5. Verify 'Login to your account' is visible
+        cy.get('[data-qa="login-email"]').type('zebriukas@example.com');
+        cy.get('[data-qa="login-password"]').type('zebriukas'); // 6. Enter correct email address and password
+        cy.get('[data-qa="login-button"]').click(); // 7. Click 'login' button
+        cy.contains('Logged in as zebriukas').should('be.visible'); // 8. Verify that 'Logged in as username' is visible
+        cy.contains('Delete Account').click(); // 9. Click 'Delete Account' button
+        cy.contains('Account Deleted!').should('be.visible'); // 10. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
     });
 });
