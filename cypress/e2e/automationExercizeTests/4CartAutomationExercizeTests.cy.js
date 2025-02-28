24515ac2f079871af6940bd2c2e1fe1de677bcb6
GazJack <greta.jackaite@gmail.com>
2025-02-28 15:28:27 +0200

    1: 19, 20, 21, 22, 25

--- cypress/e2e/automationExercizeTests/4CartAutomationExercizeTests.cy.js
+++ cypress/e2e/automationExercizeTests/4CartAutomationExercizeTests.cy.js
@@ -1,21 +1,7 @@
-//cia kai divai divuose yra ir norim nusirodyti kelia: cy.get('ul.todo-list li')
-// .each(($el,$list) => {
-//     //turetume galimybe atlikti kazkokius tai veiksmus su individualiu saraso elementu
-//     //ar elementas netuscias ir t.t.
-
-//     //Paspausti kiekviena saraso elementa double click
-//     $el.dblclick();
-// })
-// .then(($list) => {
-//     expect($list).to.have.length(6);
-// })
-
-
 // Test Case 19: View & Cart Brand Products
 describe('Test Case 19: View & Cart Brand Products', () => {
     it("View & Cart Brand Products", () => {
-        cy.visit('https://automationexercise.com');
-        cy.contains('Home').should('be.visible');
+        cy.homePage();
         cy.contains('Products').click();
         cy.contains('h2', 'Brands').should('be.visible');
         cy.contains('Polo').click();
@@ -28,8 +14,7 @@ describe('Test Case 19: View & Cart Brand Products', () => {
 // Test Case 20: Search Products and Verify Cart After Login
 describe('Test Case 20: Search Products and Verify Cart After Login', () => {
     it("Search Products and Verify Cart After Login", () => {
-        cy.visit('https://automationexercise.com');
-        cy.contains('Home').should('be.visible');
+        cy.homePage();
         cy.contains('Products').click();
         cy.contains('h2', 'All Products').should('be.visible');
         cy.get('#search_product').type('jeans');
@@ -48,8 +33,7 @@ describe('Test Case 20: Search Products and Verify Cart After Login', () => {
         cy.get('.single-products').eq(2).find('a').contains('Add to cart').click({ force: true });
         cy.get('#cartModal').should('be.visible');
         cy.get('#cartModal').contains('Continue Shopping').click({ force: true });
-        // sitas budas geresnis: cy.get("ul.nav > li > a[href='/view_cart']").click();
-        cy.contains('Cart').click();
+        cy.get("ul.nav > li > a[href='/view_cart']").click();
         cy.get('#cart_info #cart_info_table .cart_quantity').should('have.length', 3);
         cy.get('#cart_info #cart_info_table .cart_quantity').each(($el) => {
             cy.wrap($el).should('be.visible');
@@ -58,7 +42,7 @@ describe('Test Case 20: Search Products and Verify Cart After Login', () => {
         cy.get('[data-qa="login-email"]').type('zebre@example.com');
         cy.get('[data-qa="login-password"]').type('pass');
         cy.get('[data-qa="login-button"]').click();
-        cy.contains('Cart').click();
+        cy.get("ul.nav > li > a[href='/view_cart']").click();
         cy.get('#cart_info #cart_info_table .cart_quantity').should('have.length', 3);
         cy.get('#cart_info #cart_info_table .cart_quantity').each(($el) => {
             cy.wrap($el).should('be.visible');
@@ -69,14 +53,11 @@ describe('Test Case 20: Search Products and Verify Cart After Login', () => {
 
 // Test Case 21: Add review on product
 describe('Test Case 21: Add review on product', () => {
-    it.only("Add review on product", () => {
-        cy.visit('https://automationexercise.com');
-        cy.contains('Home').should('be.visible');
+    it("Add review on product", () => {
+        cy.homePage();
         cy.contains('Products').click();
         cy.contains('h2', 'All Products').should('be.visible');
-        // norėčiau geresnį kelią rasti (suknele View product):
         cy.get('a[href="/product_details/4"]').contains('View Product').click();
-        // cy.get(':nth-child(6) > .product-image-wrapper > .choose > .nav > li > a').contains('View Product').click();
         cy.contains('Write Your Review').should('be.visible');
         cy.get('#name').type('Zebrė');
         cy.get('#email').type('zebre@example.com');
@@ -88,12 +69,10 @@ describe('Test Case 21: Add review on product', () => {
 
 // Test Case 22: Add to cart from Recommended 
 describe('Test Case 22: Add to cart from Recommended ', () => {
-    it.only("Add to cart from Recommended ", () => {
-        cy.visit('https://automationexercise.com');
-        cy.contains('Home').should('be.visible');
+    it("Add to cart from Recommended ", () => {
+        cy.homePage();
         cy.get('.recommended_items').contains('recommended items').should('be.visible');
         cy.get('[data-product-id="2"]').contains('Add to cart').click();
-        // kaip paspausti antrajį produktą?
         cy.get('.text-center').contains('View Cart').click();
         cy.get('#cart_info').should('have.length', '1');
     })
@@ -102,8 +81,7 @@ describe('Test Case 22: Add to cart from Recommended ', () => {
 // Test Case 25: Verify Scroll Up using 'Arrow' button and Scroll Down functionality
 describe('Test Case 22: Add to cart from Recommended ', () => {
     it("Add to cart from Recommended ", () => {
-        cy.visit('https://automationexercise.com');
-        cy.contains('Home').should('be.visible');
+        cy.homePage();
         cy.scrollTo("bottom");
         cy.get('.single-widget').contains('Subscription').should('be.visible');
         cy.get('#scrollUp').click();
