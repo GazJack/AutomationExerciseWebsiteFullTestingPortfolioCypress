fd2a98ef9c8cad764aa0401ec8470b38725fc850
GazJack <greta.jackaite@gmail.com>
2025-03-21 15:56:19 +0200

    API 14 done

--- cypress/e2e/APItests/API.cy.js
+++ cypress/e2e/APItests/API.cy.js
@@ -1,204 +1,202 @@
 describe('API 1', () => {
-    it('Get All Products List', () => {
-      })
-    });
+  it('Get All Products List', () => {
+  })
+});
 
-  describe('API 2', () => {
-    it('Post All Products List', () => {
-        cy.request('POST', 'https://automationexercise.com/api/productsList').then((response) => {
-            const responseBody = JSON.parse(response.body);
-            expect(responseBody).to.have.property('responseCode', 405);
-            expect(responseBody).to.have.property('message', 'This request method is not supported.');
-        })
-     })
-  });
-
-  describe('API 3', () => {
-    it('Get All Brands List', () => {
-      cy.request('GET', 'https://automationexercise.com/api/brandsList').then((response) => {
-        const responseBody = JSON.parse(response.body);
-        expect(responseBody).to.have.property('responseCode', 200);
-        expect(responseBody.brands).to.be.an('array').that.is.not.empty;
-        responseBody.brands.forEach((brand) => {
-            expect(brand).to.have.property('id');
-            expect(brand).to.have.property('brand');
-        })
-      })
-    })
-  });
-
-describe('API 4', () => {
-  it('PUT To All Brands List', () => {
-  cy.request('PUT', 'https://automationexercise.com/api/brandsList').then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 405);
-    expect(responseBody).to.have.property('message', 'This request method is not supported.');
+describe('API 2', () => {
+  it('Post All Products List', () => {
+    cy.request('POST', 'https://automationexercise.com/api/productsList').then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 405);
+      expect(responseBody).to.have.property('message', 'This request method is not supported.');
     })
   })
 });
 
-// cia gal per daug testu pagal uzduoti?
-// susirasyti paaiskinimus:
+describe('API 3', () => {
+  it('Get All Brands List', () => {
+    cy.request('GET', 'https://automationexercise.com/api/brandsList').then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 200);
+      expect(responseBody.brands).to.be.an('array').that.is.not.empty;
+      responseBody.brands.forEach((brand) => {
+        expect(brand).to.have.property('id');
+        expect(brand).to.have.property('brand');
+      })
+    })
+  })
+});
+
+describe('API 4', () => {
+  it('PUT To All Brands List', () => {
+    cy.request('PUT', 'https://automationexercise.com/api/brandsList').then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 405);
+      expect(responseBody).to.have.property('message', 'This request method is not supported.');
+    })
+  })
+});
+
 describe('API 5: POST To Search Product', () => {
   it('Should return searched products list', () => {
-      const searchTerm = 'dress';
+    const searchTerm = 'dress';
+    cy.request({
+      method: 'POST',
+      url: 'https://automationexercise.com/api/searchProduct',
+      body: { search_product: searchTerm },
+      form: true
+    }).then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 200);
+      expect(responseBody.products).to.be.an('array').that.is.not.empty;
 
-      cy.request({
-          method: 'POST',
-          url: 'https://automationexercise.com/api/searchProduct',
-          body: { search_product: searchTerm },
-          form: true // Užtikrina, kad duomenys siunčiami kaip formos duomenys
-      }).then((response) => {
-          const responseBody = JSON.parse(response.body);
-          expect(responseBody).to.have.property('responseCode', 200);
-          expect(responseBody.products).to.be.an('array').that.is.not.empty;
+      responseBody.products.forEach((product) => {
+        expect(product).to.have.property('id').that.is.a('number');
+        expect(product).to.have.property('name').that.is.a('string');
+        expect(product).to.have.property('price').that.is.a('string');
+        expect(product).to.have.property('brand').that.is.a('string');
+        expect(product).to.have.property('category').that.is.an('object');
+        expect(product.category).to.have.property('usertype').that.is.an('object');
+        expect(product.category.usertype).to.have.property('usertype').that.is.a('string');
+        expect(product.category).to.have.property('category').that.is.a('string');
 
-          responseBody.products.forEach((product) => {
-              expect(product).to.have.property('id').that.is.a('number');
-              expect(product).to.have.property('name').that.is.a('string');
-              expect(product).to.have.property('price').that.is.a('string');
-              expect(product).to.have.property('brand').that.is.a('string');
-              expect(product).to.have.property('category').that.is.an('object');
-              expect(product.category).to.have.property('usertype').that.is.an('object');
-              expect(product.category.usertype).to.have.property('usertype').that.is.a('string');
-              expect(product.category).to.have.property('category').that.is.a('string');
-
-              // Patikriname, ar paieškos žodis yra produkto pavadinime arba kategorijoje
-              const nameIncludesSearch = product.name.toLowerCase().includes(searchTerm);
-              const categoryIncludesSearch = product.category.category.toLowerCase().includes(searchTerm);
-              expect(nameIncludesSearch || categoryIncludesSearch).to.be.true;
-            });
-          });
-        });
+        const nameIncludesSearch = product.name.toLowerCase().includes(searchTerm);
+        const categoryIncludesSearch = product.category.category.toLowerCase().includes(searchTerm);
+        expect(nameIncludesSearch || categoryIncludesSearch).to.be.true;
       });
+    });
+  });
+});
 
 describe('API 6', () => {
   it('POST To Search Product without search_product parameter', () => {
-  cy.request('POST', 'https://automationexercise.com/api/searchProduct').then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 400);
-    expect(responseBody).to.have.property('message', 'Bad request, search_product parameter is missing in POST request.');
+    cy.request('POST', 'https://automationexercise.com/api/searchProduct').then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 400);
+      expect(responseBody).to.have.property('message', 'Bad request, search_product parameter is missing in POST request.');
+    })
   })
-})
 });
 
 describe('API 7', () => {
   it('POST To Verify Login with valid details', () => {
-  cy.request({
-    method: 'POST',
-    url: 'https://automationexercise.com/api/verifyLogin',
-    body: {
-      email: 'john.smith2@gmail.com',
-      password: 'passs'
-    },
-    form: true
-  }).then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 200);
-    expect(responseBody).to.have.property('message', 'User exists!');
-     })
+    cy.request({
+      method: 'POST',
+      url: 'https://automationexercise.com/api/verifyLogin',
+      body: {
+        email: 'john.smith2@gmail.com',
+        password: 'passs'
+      },
+      form: true
+    }).then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 200);
+      expect(responseBody).to.have.property('message', 'User exists!');
+    })
   })
 });
 
 describe('API 8', () => {
   it('POST To Verify Login without email parameter', () => {
-  cy.request({
-    method: 'POST', 
-    url: 'https://automationexercise.com/api/verifyLogin',
-    body: {
-      password: 'passs'
-    },
-    form: true
-  }).then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 400);
-    expect(responseBody).to.have.property('message', 'Bad request, email or password parameter is missing in POST request.');
-     })
+    cy.request({
+      method: 'POST',
+      url: 'https://automationexercise.com/api/verifyLogin',
+      body: {
+        password: 'passs'
+      },
+      form: true
+    }).then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 400);
+      expect(responseBody).to.have.property('message', 'Bad request, email or password parameter is missing in POST request.');
+    })
   })
 });
 
 describe('API 9', () => {
   it('DELETE To Verify Login', () => {
-  cy.request('DELETE', 'https://automationexercise.com/api/verifyLogin').then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 405);
-    expect(responseBody).to.have.property('message', 'This request method is not supported.');
-     })
+    cy.request('DELETE', 'https://automationexercise.com/api/verifyLogin').then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 405);
+      expect(responseBody).to.have.property('message', 'This request method is not supported.');
+    })
   })
 });
 
 describe('API 10', () => {
   it('POST To Verify Login with invalid details', () => {
-  cy.request({
-    method: 'POST', 
-    url: 'https://automationexercise.com/api/verifyLogin',
-    body: {
-      email: 'john.smiths@gmail.com',
-      password: 'pass'
-    },
-    form: true
-  }).then((response) => {
-    const responseBody = JSON.parse(response.body);
-expect(responseBody).to.have.property('responseCode', 404);
-expect(responseBody).to.have.property('message', 'User not found!');
-     })
+    cy.request({
+      method: 'POST',
+      url: 'https://automationexercise.com/api/verifyLogin',
+      body: {
+        email: 'john.smiths@gmail.com',
+        password: 'pass'
+      },
+      form: true
+    }).then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 404);
+      expect(responseBody).to.have.property('message', 'User not found!');
+    })
   })
 });
 
 describe('API 11', () => {
   it('POST To Create/Register User Account', () => {
-  cy.request({
-    method: 'POST', 
-    url: 'https://automationexercise.com/api/createAccount',
-    body: {
-      name: ' Layla Smith2',
-      email: 'laylasmith2@gmail.com',
-      password: 'pass',
-      title: 'Mr',
-      birth_date: '28',
-      birth_month: '02',
-      birth_year: '1982',
-      firstname: 'John',
-      lastname: 'Smith',
-      company: 'Arijus',
-      address1: 'Haugetuft',
-      address2: '1',
-      country: 'United States',
-      zipcode: '269875',
-      state: 'Illinois',
-      city: 'Chicago',
-      mobile_number: '+025897456315'},
-  form: true
-}).then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 201);
-    expect(responseBody).to.have.property('message', 'User created!');
-     })
+    cy.request({
+      method: 'POST',
+      url: 'https://automationexercise.com/api/createAccount',
+      body: {
+        name: ' Layla Smith2',
+        email: 'laylasmith2@gmail.com',
+        password: 'pass',
+        title: 'Mr',
+        birth_date: '28',
+        birth_month: '02',
+        birth_year: '1982',
+        firstname: 'John',
+        lastname: 'Smith',
+        company: 'Arijus',
+        address1: 'Haugetuft',
+        address2: '1',
+        country: 'United States',
+        zipcode: '269875',
+        state: 'Illinois',
+        city: 'Chicago',
+        mobile_number: '+025897456315'
+      },
+      form: true
+    }).then((response) => {
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 201);
+      expect(responseBody).to.have.property('message', 'User created!');
+    })
   })
 });
 
 describe('API 12', () => {
   it('DELETE METHOD To Delete User Account', () => {
-  cy.request({
-    method: 'DELETE', 
-    url: 'https://automationexercise.com/api/deleteAccount',
-    body: {
-      email: 'laylasmith2@gmail.com',
-      password: 'pass'},
+    cy.request({
+      method: 'DELETE',
+      url: 'https://automationexercise.com/api/deleteAccount',
+      body: {
+        email: 'laylasmith2@gmail.com',
+        password: 'pass'
+      },
       form: true,
     }).then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 200);
-    expect(responseBody).to.have.property('message', 'Account deleted!');
-     })
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 200);
+      expect(responseBody).to.have.property('message', 'Account deleted!');
+    })
   })
 });
 
 describe('API 13', () => {
   it('PUT METHOD To Update User Account', () => {
-  cy.request({
-    method:'PUT',
-    url: 'https://automationexercise.com/api/updateAccount',
+    cy.request({
+      method: 'PUT',
+      url: 'https://automationexercise.com/api/updateAccount',
       body: {
         name: 'AlisaSmith',
         email: 'john.smith1234@gmail.com',
@@ -216,13 +214,14 @@ describe('API 13', () => {
         zipcode: '254789',
         state: 'Illinois',
         city: 'Chicago',
-        mobile_number: '+3259874563'},
-    form: true
+        mobile_number: '+3259874563'
+      },
+      form: true
     }).then((response) => {
-    const responseBody = JSON.parse(response.body);
-    expect(responseBody).to.have.property('responseCode', 200);
-    expect(responseBody).to.have.property('message', 'User updated!');
-     })
+      const responseBody = JSON.parse(response.body);
+      expect(responseBody).to.have.property('responseCode', 200);
+      expect(responseBody).to.have.property('message', 'User updated!');
+    })
   })
 });
 
@@ -256,5 +255,5 @@ describe('API 14', () => {
       expect(responseBody.user.zipcode).to.be.a('string');
     })
   })
-})
+});
 
